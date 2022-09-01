05efa630dc62352d5f97b6d703ac229b7f17e5c1
Recep Aslantas <info@recp.me>
2022-09-01 14:24:57 +0300

    Update infl.c

--- src/infl/infl.c
+++ src/infl/infl.c
@@ -18,13 +18,44 @@
 #include "huff.h"
 #include "../endian.h"
 
+static const uint8_t hufxd_len_litl[288] = {
+  8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
+  8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
+  8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
+  8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
+  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+  9,9,9,9,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8
+};
+
+static const uint8_t hufxd_len_dist[32] = {
+  5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
+};
+
+UNZ_INLINE
+UnzipResult
+infl_block(unzip_t          * __restrict stream,
+           uint8_t          * __restrict dst,
+           size_t                        dst_cap,
+           size_t           * __restrict dst_pos,
+           const huff_dec_t * __restrict litlen_dec,
+           const huff_dec_t * __restrict dist_dec) {
+  return UNZ_OK;
+}
+
 UNZ_HIDE
 int
 infl(unzip_t * __restrict stream, const uint8_t * __restrict p, uint32_t len) {
   const uint8_t *end;
   uint8_t        bfinal, btype;
+  huff_dec_t     hufxd_litl, hufxd_dist;
 
   end = p + len;
+
+  /* initilize static tables */
+  huff_dec_init(&hufxd_litl, hufxd_len_litl, ARRAY_LEN(hufxd_len_litl));
+  huff_dec_init(&hufxd_dist, hufxd_len_dist, ARRAY_LEN(hufxd_len_dist));
 
   /* TODO: option to check enough input / output memory */
   do {
@@ -40,16 +71,15 @@ infl(unzip_t * __restrict stream, const uint8_t * __restrict p, uint32_t len) {
         be_16(len,  p);
         be_16(nlen, p);
 
-        /* TODO: option to skip this */
-        if (unlikely(len != ~nlen)) { return -2; }
+        /* TODO: option to skip this or different error code? */
+        if (unlikely(len != ~nlen)) { goto err; }
 
         memcpy(stream->dst, p, len);
         continue;
       }
       case 0x2: {
         /* static huffman */
-        huff_dec_t litlen_dec, dist_dec;
-        
+      
         break;
       }
       case 0x4:
@@ -57,9 +87,12 @@ infl(unzip_t * __restrict stream, const uint8_t * __restrict p, uint32_t len) {
         break;
       default:
         /* unkown btype */
-        return -1;
+        goto err;
     }
   } while (p < end);
 
   return 0;
+
+err:
+  return -1;
 }
