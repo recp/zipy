c2bae3dfc6e510373d8bd29ef5effe6c5f7ff3e8
Recep Aslantas <info@recp.me>
2026-05-07 11:45:24 +0300

    allow fast path with unused password

--- src/main.c
+++ src/main.c
@@ -1640,7 +1640,6 @@ main(int argc, char *argv[]) {
                && !policies
                && !save_dir
                && config.options.on_conflict == ZIPY_CONFLICT_OVERWRITE
-               && !config.options.password
                && !zipy_has_encrypted(zip)
                && !zipy_has_symlink(zip);
   if (directExtract) {
